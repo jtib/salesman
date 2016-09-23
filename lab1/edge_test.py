0631d24daed9cbb1c64b2a050245f24f7095301b
Jean-Noël Weller <jeannoel.weller@aliceadsl.fr>
2016-09-23 21:35:56 +0200

    Tests unitaires sur la classe edge

---
+++ lab1/edge_test.py
@@ -0,0 +1,32 @@
+from node import Node
+from edge import EdgeException
+from edge import Edge
+
+
+import unittest
+
+class TestEdge(unittest.TestCase):
+
+    def setUp(self):
+        node1 = Node()
+        node2 = Node()
+        self.__edge = Edge(node1,node2)
+
+    def test_count(self):
+        "Verifie l'incrementation du compteur"
+        prev_edge_count = self.__edge.get_count()
+        node1 = Node()
+        node2 = Node()
+        edge2 = Edge(node1, node2)
+        self.assertEqual(prev_edge_count + 1, self.__edge.get_count())
+
+    def test_init(self):
+        node = Node()
+        with self.assertRaises(EdgeException):
+            edge = Edge(node,node)
+
+
+
+if __name__ == "__main__":
+    unittest.main()n__":
+    unittest.main()
