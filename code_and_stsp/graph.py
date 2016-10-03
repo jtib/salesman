13a9dd2ee003e920fc25da60c02136e514deb468
jtib <juliette.tib@gmail.com>
2016-10-03 15:39:15 -0400

    réorganisation des fichiers et ajout de la méthode sort_edges()

--- code_and_stsp/graph.py
+++ code_and_stsp/graph.py
@@ -12,41 +12,16 @@ class Graph(object):
         self.__edges = 0
         self.__nodes = 0
 
-    def add_node(self, node):
-        "Ajoute un noeud au graphe."
-        self.__adj.setdefault(node,{})
-        self.__nodes += 1
-
-    def retrieve_nodes_from_id(self, *ids):
-        return [node for node in self.__adj.keys()
-                if node.get_id() in ids]
-
-    def add_edge(self, edge):
-        "Ajoute une arete au graphe."
-        (n1, n2) = edge.get_nodes()
-        # retrieving nodes
-        nodes = self.retrieve_nodes_from_id(n1.get_id(), n2.get_id())
-        # if both nodes already there
-        if len(nodes) == 2:
-            self.__adj[nodes[1]][nodes[0]] = self.__adj[nodes[0]][nodes[1]]\
-                    = edge
-            self.__edges = edge.get_id() + 1 #valid for all tsps, avoids has_key
-        # if only one is there and it doesn't point at itself, or none are
-        elif len(nodes) < 2 and n1 != n2:
-            raise KeyError("Missing node(s). Add all nodes before adding edges\
-                (nodes = {0}, n1 = {1}, n2 = {2})".format(nodes, n1.get_id(), n2.get_id()))
 
     def get_name(self):
         "Donne le nom du graphe."
         return self.__name
 
+
     def get_nodes(self):
         "Donne la liste des noeuds du graphe."
         return self.__adj.keys()
 
-    def get_nb_nodes(self):
-        "Donne le nombre de noeuds du graphe."
-        return self.__nodes
 
     def get_edges(self):
         "Donne la liste des aretes du graphe."
@@ -54,14 +29,58 @@ class Graph(object):
         edges.extend([v for n in self.get_nodes() for v in self.__adj[n].values()])
         return list(set(edges)) # removing doubles
 
+
+    def get_nb_nodes(self):
+        "Donne le nombre de noeuds du graphe."
+        return self.__nodes
+
+
     def get_nb_edges(self):
         "Donne le nombre d'aretes du graphe."
         return self.__edges
 
+
+    def retrieve_nodes_from_id(self, *ids):
+        return [node for node in self.__adj.keys()
+                if node.get_id() in ids]
+
+
+    def add_node(self, node):
+        "Ajoute un noeud au graphe."
+        self.__adj.setdefault(node,{})
+        self.__nodes += 1
+
+
+    def add_edge(self, edge):
+        "Ajoute une arete au graphe."
+        (n1, n2) = edge.get_nodes()
+        # retrieving nodes
+        nodes = self.retrieve_nodes_from_id(n1.get_id(), n2.get_id())
+
+        # if both nodes already there
+        if len(nodes) == 2:
+            self.__adj[nodes[1]][nodes[0]] = self.__adj[nodes[0]][nodes[1]]\
+                    = edge
+            self.__edges = edge.get_id() + 1 #valid for all tsps, avoids has_key
+
+        # if only one is there and it doesn't point at itself, or none are
+        elif len(nodes) < 2 and n1 != n2:
+            raise KeyError("Missing node(s). Add all nodes before adding edges\
+                (nodes = {0}, n1 = {1}, n2 = {2})".format(nodes, n1.get_id(), n2.get_id()))
+
+    def sort_edges(self):
+        "Trie les aretes selon leur poids en ordre croissant."
+        edges = self.get_edges()
+        edges.sort(key=lambda edge: edge.get_weight())
+        return edges
+
+
     def __repr__(self):
+        "Affiche le graphe."
         name = self.get_name()
         nb_nodes = self.get_nb_nodes()
         nb_edges = self.get_nb_edges()
+
         s = 'Graphe %s comprenant %d noeuds et %d aretes' % (name, nb_nodes, nb_edges)
         for node in self.get_nodes():
             s += '\n  ' + repr(node)
@@ -82,5 +101,8 @@ if __name__ == '__main__':
         n2 = Node(iden=count, name='test %d' % count)
         G.add_node(n2)
         count += 1
-        G.add_edge(Edge(k, n1, n2, weight=42))
+        G.add_edge(Edge(k, n1, n2, weight=k%3))
     print G
+    print "Aretes triees par poids :"
+    edg = G.sort_edges()
+    print "\n".join([str(e) for e in edg])
