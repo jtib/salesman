590a8687689d7994926b94e7125e67d9ef63fefe
Jean-Noël Weller <jeannoel.weller@aliceadsl.fr>
2016-10-13 23:33:11 +0200

    Kruskal implémenté dans Graph: l'arbre créé est stocké dans un graphe, on se contente de faire appel à la fonction union_sets dans le "if" (renvoie faux si la modification est impossible), et on arrête la boucle dès que l'arbre est plein. On utilise ma_liste.sort() pour trier les edges (avec implémentation d'un __lt__ et __le__ dans edge).

--- code_and_stsp/graph.py
+++ code_and_stsp/graph.py
@@ -6,23 +6,47 @@ class Graph(object):
     Une classe generique pour representer un graphe comme un ensemble de
     noeuds.
     """
-
     def __init__(self, name='Sans nom'):
         self.__name = name
         self.__adj = {} # Matrice d'adjacence
         self.__edges = 0
         self.__nodes = 0
 
+    def add_node(self, node):
+        "Ajoute un noeud au graphe."
+        self.__adj.setdefault(node,{})
+        self.__nodes += 1
+
+    def retrieve_nodes_from_id(self, *ids):
+        return [node for node in self.__adj.keys()
+                if node.get_id() in ids]
+
+    def add_edge(self, edge):
+        "Ajoute une arete au graphe."
+        (n1, n2) = edge.get_nodes()
+        # retrieving nodes
+        nodes = self.retrieve_nodes_from_id(n1.get_id(), n2.get_id())
+        # if both nodes already there
+        if len(nodes) == 2:
+            self.__adj[nodes[1]][nodes[0]] = self.__adj[nodes[0]][nodes[1]]\
+                    = edge
+            self.__edges = edge.get_id() + 1
+        # if only one is there and it doesn't point at itself, or none are
+        elif len(nodes) < 2 and n1 != n2:
+            raise KeyError("Missing node(s). Add all nodes before adding edges\
+                (nodes = {0}, n1 = {1}, n2 = {2})".format(nodes, n1.get_id(), n2.get_id()))
 
     def get_name(self):
         "Donne le nom du graphe."
         return self.__name
 
-
     def get_nodes(self):
         "Donne la liste des noeuds du graphe."
         return self.__adj.keys()
 
+    def get_nb_nodes(self):
+        "Donne le nombre de noeuds du graphe."
+        return self.__nodes
 
     def get_edges(self):
         "Donne la liste des aretes du graphe."
@@ -30,64 +54,47 @@ class Graph(object):
         edges.extend([v for n in self.get_nodes() for v in self.__adj[n].values()])
         return list(set(edges)) # removing doubles
 
-
-    def get_nb_nodes(self):
-        "Donne le nombre de noeuds du graphe."
-        return len(self.get_nodes())
-
-
     def get_nb_edges(self):
         "Donne le nombre d'aretes du graphe."
-        return len(self.get_edges())
+        return self.__edges
 
+    def kruskal(self):
+        "Retourne un arbre de recouvrement minimal s'il existe"
+        min_tree = Graph('Arbre Minimal')
 
-    def retrieve_nodes_from_id(self, *ids):
-        return [node for node in self.__adj.keys()
-                if node.get_id() in ids]
+        disj_sets = {}
 
-
-    def add_node(self, node):
-        "Ajoute un noeud au graphe."
-        self.__adj.setdefault(node,{})
-        self.__nodes += 1
-
-
-    def add_edge(self, edge):
-        "Ajoute une arete au graphe."
-        (n1, n2) = edge.get_nodes()
-        # retrieving nodes
-        nodes = self.retrieve_nodes_from_id(n1.get_id(), n2.get_id())
-
-        # if both nodes already there
-        if len(nodes) == 2:
-            self.__adj[nodes[1]][nodes[0]] = self.__adj[nodes[0]][nodes[1]]\
-                    = edge
-            self.__edges = edge.get_id() + 1 #valid for all tsps, avoids has_key
-
-        # if only one is there and it doesn't point at itself, or none are
-        elif len(nodes) < 2 and n1 != n2:
-            raise KeyError("Missing node(s). Add all nodes before adding edges\
-                (nodes = {0}, n1 = {1}, n2 = {2})".format(nodes, n1.get_id(), n2.get_id()))
-
-    def sort_edges(self):
-        "Trie les aretes selon leur poids en ordre croissant."
-        edges = self.get_edges()
-        edges.sort(key=lambda edge: edge.get_weight())
-        return edges
-
-
-    def sets_from_nodes(self):
-        "Renvoie les DisjointSet formes a partir des noeuds."
         nodes = self.get_nodes()
-        return {DisjointSet(node) for node in nodes}
+        # Le nombre de noeuds du graphe
+        nb_nodes = self.__nodes
+        # On remplit le dictionnaire de disjoint_sets
+        for node in nodes:
+            disj_sets[node] = DisjointSet(node)
 
+        edges = self.get_edges()
+        # La liste est triee selon la comparaison implementee dans edge
+        edges.sort()
+
+        # Construction de l'arbre
+        for edge in edges:
+            (node1,node2) = edge.get_nodes()
+
+            # Si l'union des deux disjoint_sets est reussie
+            if disj_sets[node1].union_sets(disj_sets[node2]):
+                # On complete l'arbre minimal
+                min_tree.add_node(node1)
+                min_tree.add_node(node2)
+                min_tree.add_edge(edge)
+            # Si tous les noeuds sont dans min_tree, c'est que l'arbre est fini
+            if min_tree.get_nb_nodes() == nb_nodes:
+                break
+
+        return min_tree
 
     def __repr__(self):
-        "Affiche le graphe."
         name = self.get_name()
         nb_nodes = self.get_nb_nodes()
         nb_edges = self.get_nb_edges()
-
         s = 'Graphe %s comprenant %d noeuds et %d aretes' % (name, nb_nodes, nb_edges)
         for node in self.get_nodes():
             s += '\n  ' + repr(node)
@@ -108,8 +115,5 @@ if __name__ == '__main__':
         n2 = Node(iden=count, name='test %d' % count)
         G.add_node(n2)
         count += 1
-        G.add_edge(Edge(k, n1, n2, weight=k%3))
+        G.add_edge(Edge(k, n1, n2, weight=42))
     print G
-    print "Aretes triees par poids :"
-    edg = G.sort_edges()
-    print "\n".join([str(e) for e in edg])
